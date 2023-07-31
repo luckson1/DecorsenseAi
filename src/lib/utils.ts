690b40905feadaafd30f4ccefe828a50c9d5f41e
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-07-31 06:04:07 +0300

    fix: redis bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -80,8 +80,11 @@ export const rooms: roomType[] = [
 ];
 
 export const redis =
-  new Redis({
-        url: env.UPSTASH_REDIS_REST_URL,
-        token: env.UPSTASH_REDIS_REST_TOKEN,
+  !!process.env.UPSTASH_REDIS_REST_URL && !!process.env.UPSTASH_REDIS_REST_TOKEN
+    ? new Redis({
+        url: process.env.UPSTASH_REDIS_REST_URL,
+        token: process.env.UPSTASH_REDIS_REST_TOKEN,
       })
+    : undefined;
+
   
